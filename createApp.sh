856831f14b4acbcff52beb4b12b6b083d3b69b2d
devuser <cicddemo@amazon.com>
2018-07-09 09:03:13 -0700

    fixes

--- createApp.sh
+++ createApp.sh
@@ -130,7 +130,7 @@ git push origin master
 # Creating Stack
 #
 echo "Creating CloudFormation Stack"
-AWSCF="$(aws cloudformation list-stacks --region ${AWSREGION} | grep webappdotnetcore2)"
+AWSCF="$(aws cloudformation list-stacks --region ${AWSREGION} | grep ${STACKNAME})"
 if [[ "${#AWSFC}" > 1 ]]; then
    aws cloudformation delete-stack --stack-name ${STACKNAME} \
       --region ${AWSREGION}
